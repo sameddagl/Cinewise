ff401a6e7b0135a6b8a8397306c0dd4d68bf389e
Samed Dağlı <samedddagl@gmail.com>
2023-01-25 12:48:30 +0300

    Add splash screen and create main three screens

---
+++ Cinewise/Helper/Constants.swift
@@ -0,0 +1,17 @@
+//
+//  Constants.swift
+//  Cinewise
+//
+//  Created by Samed Dağlı on 25.01.2023.
+//
+
+import Foundation
+import UIKit
+
+enum Images {
+    static let splashImage = UIImage(named: "SplashImage")
+}
+
+enum Colors {
+    static let mainColor = UIColor(named: "MainColor")
+}
---
+++ Cinewise/Screens/Home/HomeViewController.swift
@@ -0,0 +1,26 @@
+//
+//  HomeViewController.swift
+//  Cinewise
+//
+//  Created by Samed Dağlı on 25.01.2023.
+//
+
+import UIKit
+
+final class HomeViewController: UIViewController {
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+}
+
+//MARK: - UI Related
+extension HomeViewController {
+    private func layout() {
+        configureView()
+    }
+    
+    private func configureView() {
+        view.backgroundColor = Colors.mainColor
+    }
+}
+
